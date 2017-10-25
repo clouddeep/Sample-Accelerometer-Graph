b659467f2909018724bec598a6010a87c5b9d6df
Tuan Shou Cheng <shoujhengduan@gmail.com>
2017-10-25 08:18:47 +0800

    Initial Commit

---
+++ Accelerometer Graph/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Accelerometer Graph
+//
+//  Created by Tuan Shou Cheng on 2017/10/25.
+//  Copyright © 2017年 Tuan Shou Cheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Accelerometer Graph/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Accelerometer Graph
+//
+//  Created by Tuan Shou Cheng on 2017/10/25.
+//  Copyright © 2017年 Tuan Shou Cheng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
